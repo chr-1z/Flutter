70704f53bb71c93a1f18ed40ac6240c05cf8748f
Christian Leichtweis <chris99leichtweiss@gmail.com>
2021-03-23 11:54:17 -0300

    Update imc.dart

--- Projects/Section4-ProgramStructure/imc.dart
+++ Projects/Section4-ProgramStructure/imc.dart
@@ -11,24 +11,29 @@ main() {
 
 calculoImc() {
   print("=== Digite seu peso ===");
-  var textPeso = stdin.readLineSync()!;
-  var peso = int.parse(textPeso);
+  String textPeso = stdin.readLineSync()!;
+  int peso = int.parse(textPeso);
 
   print("=== Digite sua altura (em M, usar ponto) ===");
-  var textAltura = stdin.readLineSync()!;
-  var altura = double.parse(textAltura);
+  String textAltura = stdin.readLineSync()!;
+  double altura = double.parse(textAltura);
 
-  var calcImc = peso / (altura * altura);
+  double calcImc = peso / (altura * altura);
+  print(calcImc);
 
+  imprimirResultado(calcImc);
+}
+
+imprimirResultado(double calcImc) {
   if (calcImc < 18.5) {
     print("Abaixo do peso!");
-  } else if (calcImc >= 18.5 && calcImc <= 24.9) {
+  } else if (calcImc >= 18.5 && calcImc <= 24.99) {
     print("Peso normal");
-  } else if (calcImc >= 25 && calcImc <= 29.9) {
+  } else if (calcImc >= 25 && calcImc <= 29.99) {
     print("Sobrepeso");
-  } else if (calcImc >= 30 && calcImc <= 34.9) {
+  } else if (calcImc >= 30 && calcImc <= 34.99) {
     print("Obesidade grau 1");
-  } else if (calcImc >= 35 && calcImc <= 39.9) {
+  } else if (calcImc >= 35 && calcImc <= 39.99) {
     print("Obesidade grau 2");
   } else {
     print("Obesidade grau 3");
