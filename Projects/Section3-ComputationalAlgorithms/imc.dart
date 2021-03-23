063ba3503062c7d40547d19aca9a3e0f1cb7e6a3
Christian Leichtweis <chris99leichtweiss@gmail.com>
2021-03-23 11:54:38 -0300

    Update imc.dart

--- Projects/Section3-ComputationalAlgorithms/imc.dart
+++ Projects/Section3-ComputationalAlgorithms/imc.dart
@@ -18,13 +18,13 @@ main() {
 
   if (calcImc < 18.5) {
     print("Abaixo do peso!");
-  } else if (calcImc >= 18.5 && calcImc <= 24.9) {
+  } else if (calcImc >= 18.5 && calcImc <= 24.99) {
     print("Peso normal");
-  } else if (calcImc >= 25 && calcImc <= 29.9) {
+  } else if (calcImc >= 25 && calcImc <= 29.99) {
     print("Sobrepeso");
-  } else if (calcImc >= 30 && calcImc <= 34.9) {
+  } else if (calcImc >= 30 && calcImc <= 34.99) {
     print("Obesidade grau 1");
-  } else if (calcImc >= 35 && calcImc <= 39.9) {
+  } else if (calcImc >= 35 && calcImc <= 39.99) {
     print("Obesidade grau 2");
   } else {
     print("Obesidade grau 3");
