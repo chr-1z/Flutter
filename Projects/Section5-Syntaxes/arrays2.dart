ad85913a3f2765b747b7bb252dd15348e3781fdf
Christian Leichtweis <chris99leichtweiss@gmail.com>
2021-03-23 16:49:01 -0300

    Update arrays2.dart

--- Projects/Section5-Syntaxes/arrays2.dart
+++ Projects/Section5-Syntaxes/arrays2.dart
@@ -1,12 +1,23 @@
 import 'dart:io';
 
 main() {
-  var nomes = ["Christian", "Ana", "Paulo", "Jordana"];
+  List<String> nomes = ["Christian", "Ana", "Paulo", "Jordana"];
 
+  nomes.add("Teste");
   print(nomes.length);
+  print(nomes);
 
-  
+  nomes.add("mais1");
+  print(nomes.length);
+  print(nomes);
 
+  print(nomes[5]);
 
+  nomes.removeLast();
+  print(nomes.length);
+  print(nomes);
 
+  nomes.remove("Christian");
+  print(nomes.length);
+  print(nomes);
 }
