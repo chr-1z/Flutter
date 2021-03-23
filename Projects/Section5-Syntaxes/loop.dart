775e799d0cbcda9c3392080373d2b7290fdba7e4
Christian Leichtweis <chris99leichtweiss@gmail.com>
2021-03-23 14:49:31 -0300

    upload

--- Projects/Section5-Syntaxes/loop.dart
+++ Projects/Section5-Syntaxes/loop.dart
@@ -1,5 +1,16 @@
 main() {
-  for (int x = 0; x < 10; x++) {
-    print("Oi $x");
+  // for (int x = 0; x < 10; x++) {
+  //   print("Oi $x");
+  // }
+
+  bool condicao = true;
+  int x = 1;
+
+  while (condicao) {
+    print("rodou pela $x" + "ª vez");
+    if (x > 9) {
+      condicao = false;
+    }
+    x++;
   }
 }
