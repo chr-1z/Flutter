8c838036aa83e223788a1c065cf3ff4ee991bb36
Christian Leichtweis <chris99leichtweiss@gmail.com>
2021-03-23 10:24:39 -0300

    aula 18 - primeiro algoritmo

--- Projects/Section3-ComputationalAlgorithms/hello.dart
+++ Projects/Section3-ComputationalAlgorithms/hello.dart
@@ -1,7 +1,76 @@
 main() {
-  var text1 = "Christian ";
-  var text2 = "Oliveira";
+  // pergunta a idade da pessoa;
+  // se a idade for maior ou igual a 18;
+  // ele é maior de idade;
+  // se não for;
+  // ele não é maior de idade;
 
-  var total = text1 + text2;
-  print(total);
+  var idade = 22;
+
+  if (idade >= 18) {
+    print("maior de idade");
+  } else {
+    print("ele não é maior de idade");
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// main() {
+//   var num1 = 3;
+//   var num2 = "eqwe";
+
+//   var isTrue = num1.runtimeType == num2.runtimeType; //se são o mesmo tipo
+
+//   if (isTrue) {
+//     print('Isso é verdade');
+//   } else {
+//     print("Não é verdadeiro");
+//   }
+// }
+
+
