6e54bd9e609f535267672c4c9fcfec316e688265
Christian Leichtweis <chris99leichtweiss@gmail.com>
2021-03-24 14:43:16 -0300

    aula 46

--- Projects/Section7-Maps/maps.dart
+++ Projects/Section7-Maps/maps.dart
@@ -1,13 +1,48 @@
+import 'dart:io';
+
+Map<String, dynamic> cadastro = {};
+
 main() {
-  Map<String, dynamic> maps = {
-    "nome": "Christian José",
-    "idade": 21,
-    "cidade": "Palmas",
-    "estado": "Tocantins"
-  };
+  // Map<String, dynamic> maps = {
+  //   "nome": "Christian José",
+  //   "idade": 21,
+  //   "cidade": "Palmas",
+  //   "estado": "Tocantins"
+  // };
 
-  //maps["nome"] = "João Alves";
-  print(maps["nome"]);
+  // //maps["nome"] = "João Alves";
+  // print(maps["nome"]);
 
+  bool condicao = true;
+  print("\x1B[2J\x1B[0;0H");
 
+  while (condicao) {
+    print("=== DIGITE UM COMANDO ===");
+    String comando = stdin.readLineSync()!;
+    if (comando == "sair") {
+      print("=== PROGRAMA FINALIZADO ===");
+      condicao = false;
+    } else if (comando == "cadastro") {
+      print("\x1B[2J\x1B[0;0H");
+      cadastrar();
+    } else if (comando == "imprimir") {
+      print(cadastro);
+    } else {
+      print(" === ESSE COMANDO NÃO EXISTE === ");
+    }
+  }
+}
+
+cadastrar() {
+  print(" === Digite o seu nome === ");
+  cadastro["nome"] = stdin.readLineSync();
+
+  print(" === Digite a sua idade === ");
+  cadastro["idade"] = stdin.readLineSync();
+
+  print(" === Digite a sua cidade === ");
+  cadastro["cidade"] = stdin.readLineSync();
+
+  print(" === Digite o seu estado ===");
+  cadastro["estado"] = stdin.readLineSync();
 }
