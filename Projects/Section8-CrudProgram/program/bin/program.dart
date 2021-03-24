1c0cb7b8ca206e66d4f0f9de61f985632db2f9a5
Christian Leichtweis <chris99leichtweiss@gmail.com>
2021-03-24 17:45:59 -0300

    Delete Projects/Section8-CrudProgram/program directory

--- Projects/Section8-CrudProgram/program/bin/program.dart
+++
@@ -1,5 +0,0 @@
-import 'package:program/program.dart' as program;
-
-void main(List<String> arguments) {
-  print('Hello world: ${program.calculate()}!');
-}
--- Projects/Section8-CrudProgram/program/test/program_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:program/program.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
