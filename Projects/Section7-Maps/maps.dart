35686afbec7f35bc02cab91f62801d615478cd04
Christian Leichtweis <chris99leichtweiss@gmail.com>
2021-03-24 14:13:44 -0300

    aula 44

---
+++ Projects/Section7-Maps/maps.dart
@@ -0,0 +1,13 @@
+main() {
+  var maps = {
+    "nome": "Christian José",
+    "idade": 21,
+    "cidade": "Palmas",
+    "estado": "Tocantins"
+  };
+
+  //maps["nome"] = "João Alves";
+  print(maps["nome"]);
+
+  
+}
